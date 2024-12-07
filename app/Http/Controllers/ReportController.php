9d180cdda87dd8ca24f1314a25d57bbb0330bb95
techline <techline@hotmail.it>
2024-12-07 16:36:17 +0100

    report

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ReportController extends Controller
+{
+    
+    public function generateReport()
+    {
+        $totalOrders = Order::count();
+        $totalProductsSold = Order::join('order_products', 'orders.id', '=', 'order_products.order_id')
+            ->sum('order_products.quantity');
+
+        $totalRevenue = Order::join('order_products', 'orders.id', '=', 'order_products.order_id')
+            ->join('products', 'order_products.product_id', '=', 'products.id')
+            ->sum(DB::raw('order_products.quantity * products.price'));
+
+        return response()->json([
+            'total_orders' => $totalOrders,
+            'total_products_sold' => $totalProductsSold,
+            'total_revenue' => $totalRevenue,
+        ]);
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,7 @@ Route::get('/products/{product}', [ProductController::class, 'show']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('orders', OrderController::class)->only(['index', 'update', 'destroy']);
     Route::resource('products', ProductController::class)->except(['index', 'show']);
+    Route::get('reports',[ReportController::class,'generateReport'])->name('reports.generate');
 });
 
 
